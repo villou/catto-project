16ba2c88abf44cfb866ae1b6a54bb707153c202e
Villou <dkulic01@gmail.com>
2022-01-06 19:50:54 +0100

    add context and provider in program.cs

--- catto/Program.cs
+++ catto/Program.cs
@@ -1,8 +1,15 @@
-using Microsoft.AspNetCore.Builder;
+using catto.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CattoContext>();
+builder.Services.AddTransient<UserProvider>();
+
 var app = builder.Build();
+
 app.UseSwagger();
 app.MapGet("/", () => "Hello World!");
 app.UseSwaggerUI();
