07b1c8f7f239bc56dd0890f83866121b9280a496
Villou <dkulic01@gmail.com>
2022-01-06 19:51:53 +0100

    update controllers

--- catto/Controllers/ScoreController.cs
+++ catto/Controllers/ScoreController.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using catto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace catto.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[api/controller]")]
 public class ScoreController : ControllerBase
 {
+    private readonly CattoContext _context;
+
+    public ScoreController(CattoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public List<Score> Get()
+    {
+        return _context.Scores.ToList();
+    }
+
+    // GET api/score/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<List<Score>>> GetScoreByUserId(int userId)
+    {
+        return await _context.Scores.Where(s => s.UserId == userId).ToListAsync();
+    }
 }
--- catto/Controllers/UserController.cs
+++ catto/Controllers/UserController.cs
@@ -1,9 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using catto.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace catto.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[api/controller]")]
 public class UserController : ControllerBase
 {
+
+  private readonly CattoContext _context;
+  private readonly UserProvider _userProvider;
+
+  public UserController(CattoContext context, UserProvider userProvider)
+  {
+    _context = context;
+    _userProvider = userProvider;
+  }
+
+  [HttpGet("me")]
+  public async Task<ActionResult<User>> GetMe()
+  {
+    var user = await _userProvider.GetUserFromToken(HttpContext);
+    return user == null ? Unauthorized() : user;
+  }
+
+  // GET: api/User
+  [HttpGet]
+  public List<User> Get()
+  {
+    return _context.Users.ToList();
+  }
+
+  // GET: api/User/5
+  [HttpGet("{id}")]
+  public async Task<ActionResult<User>> GetUserById(int id)
+  {
+    var user = await _context.Users.FindAsync(id);
+
+    if (user == null)
+    {
+      return NotFound();
+    }
+
+    return user;
+
+  }
+
+  [HttpPatch]
+  public async Task<ActionResult<User>> UpdateUser(User user)
+  {
+    _context.Entry(user).State = EntityState.Modified;
+    await _context.SaveChangesAsync();
+    return user;
+  }
+
+
+  // créer un dto avec uniquement les champs qui sont requis (username, password)
+  [HttpPost("login")]
+  public async Task<ActionResult<User>> Login([FromBody] User user)
+  {
+    var userLogin = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username && u.Password == user.Password);
+    if (userLogin == null)
+    {
+      return NotFound();
+    }
+    return userLogin;
+  }
+
+  [HttpPost("register")]
+  public async Task<ActionResult<User>> Register(User user)
+  {
+    var createdUser = await _userProvider.Register(user);
+
+    var token = await _context.Tokens.AddAsync(Token.GenerateToken(createdUser.Id));
+
+    HttpContext.Response.Cookies.Append("Token", token.Entity.TokenKey);
+
+    return createdUser;
+  }
+
+  [HttpPost("logout")]
+  public async Task<ActionResult<User>> Logout()
+  {
+    return null;
+  }
+
 }
