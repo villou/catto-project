09b4c4722c9635dfd18f1231d9b150eab3ea9a05
Villou <dkulic01@gmail.com>
2022-01-07 23:45:48 +0100

    create DTO for user

---
+++ catto/DTO/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using catto.Models;
+
+namespace catto.DTO;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public static UserDto FromUser(User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Username = user.Username,
+            Password = user.Password
+        };
+    }
+}
