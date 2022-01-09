ce1a7c44f30345ee1a42d0e45f50b61f5769d661
Villou <dkulic01@gmail.com>
2022-01-09 01:15:14 +0100

    add context in front

--- catto/ClientApp/proxy.conf.js
+++ catto/ClientApp/proxy.conf.js
@@ -1,16 +1,17 @@
-const { env } = require('process');
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:8559';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(";")[0]
+  : "http://localhost:8559";
 
 const PROXY_CONFIG = [
   {
-    context: [
-      "/weatherforecast",
-   ],
+    context: ["/weatherforecast", "/api"],
     target: target,
-    secure: false
-  }
-]
+    secure: false,
+  },
+];
 
 module.exports = PROXY_CONFIG;
