dcc81190b237eccb083195de59133c31857d71fc
Villou <dkulic01@gmail.com>
2022-01-14 21:30:03 +0100

    create type Image

---
+++ catto/ClientApp/src/app/model/image.ts
@@ -0,0 +1,20 @@
+export type Image = {
+  url: string;
+  isCat: boolean;
+};
+
+export const imageStack: Image[] = [
+  { url: 'https://i.imgur.com/81qoDlx.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/cMABkea.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/xdyjKN8.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/0ZDH83u.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/mdkCxgY.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/ctUAN7I.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/j7KXR98.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/qmC7uhX.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/t5oeyLo.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/dfoiKzY.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/3L5GuRP.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/FQMNlA9.jpeg', isCat: true },
+  { url: 'https://i.imgur.com/Ppv7ArW.jpeg', isCat: true },
+];
