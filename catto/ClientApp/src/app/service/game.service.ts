e3b5db1e759719eb3380389bd3a093e62f66710d
Villou <dkulic01@gmail.com>
2022-01-15 23:08:45 +0100

    create game service

--- catto/ClientApp/src/app/service/game.service.ts
+++ catto/ClientApp/src/app/service/game.service.ts
@@ -1,8 +1,58 @@
+import { imageBase } from './../model/image';
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
+import { TimerService } from './timer.service';
+import { Image } from './../model/image';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  constructor() {}
+  imagesStack: Image[] = [];
+  imagesDone: Image[] = [];
+  currentImage?: Image;
+  get score() {
+    return this.imagesDone?.length;
+  }
+
+  constructor(
+    private timerService: TimerService,
+    private authService: AuthService
+  ) {
+    timerService.onFinish.subscribe(() => {
+      this.endGame();
+    });
+  }
+
+  public startGame() {
+    this.shuffle();
+    this.popStack();
+    this.timerService.startTimer(15);
+  }
+
+  public endGame() {
+    this.timerService.stopTimer();
+  }
+
+  public setDone() {
+    if (!this.currentImage) {
+      return;
+    }
+    this.imagesDone.push(this.currentImage);
+    this.popStack();
+  }
+
+  public popStack() {
+    this.currentImage = this.imagesStack.pop();
+  }
+
+  public shuffle() {
+    this.imagesStack = shuffleArray(imageBase);
+  }
 }
+
+const shuffleArray = <T>(array: T[]): T[] =>
+  array
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
