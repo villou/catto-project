ac860fae777a3407bbbc99f8b35e2cb4d82b2c4a
Villou <dkulic01@gmail.com>
2022-01-17 18:36:31 +0100

    create score dto

---
+++ catto/DTO/ScoreDto.cs
@@ -0,0 +1,12 @@
+﻿namespace catto.DTO;
+
+public class ScoreDto
+{
+    public int Value { get; set; }
+}
+
+public class ScoreIndexDto
+{
+    public int Score { get; set; }
+    public string Username { get; set; }
+}
