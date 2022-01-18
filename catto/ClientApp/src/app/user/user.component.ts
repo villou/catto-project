431d18f88cb04992d24bda97820c7eb4256667e3
Villou <dkulic01@gmail.com>
2022-01-18 21:48:36 +0100

    Input for user comp

--- catto/ClientApp/src/app/user/user.component.ts
+++ catto/ClientApp/src/app/user/user.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  //@Input('avatar') avatar?: string;
+  @Input('username') username?: string;
+  @Input('score') score?: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
