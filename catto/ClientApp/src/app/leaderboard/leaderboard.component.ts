e83eb37ad1dd19f4efb628482b274d9db7736386
Villou <dkulic01@gmail.com>
2022-01-18 21:47:28 +0100

    leaderboard (wip)

--- catto/ClientApp/src/app/leaderboard/leaderboard.component.ts
+++ catto/ClientApp/src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,32 @@
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Score } from '../model/score';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.css']
+  styleUrls: ['./leaderboard.component.css'],
 })
 export class LeaderboardComponent implements OnInit {
+  scores: Score[] = [];
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  ngOnInit(): void {
+  constructor(public authService: AuthService, private http: HttpClient) {
+    this.getUsersScore();
   }
 
+  getUsersScore() {
+    this.http.get<Score[]>('api/Score').subscribe(
+      (result) => {
+        this.scores = result;
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  ngOnInit(): void {}
 }
