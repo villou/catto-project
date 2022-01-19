44eb7e0a1960194658f44c098e422ec0adffa474
Villou <dkulic01@gmail.com>
2022-01-20 00:00:25 +0100

    add test setup

---
+++ catto-test/ScoreControllerTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace catto_test;
+
+public class ScoreControllerTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ catto-test/UserControllerTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace catto_test;
+
+public class UserControllerTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
