fbc617a42f27c2b7b344ad169468c2a9aaa3dfd9
Villou <dkulic01@gmail.com>
2022-01-20 10:59:22 +0100

    setup ScoreControllerTest

--- catto-test/ScoreControllerTest.cs
+++ catto-test/ScoreControllerTest.cs
@@ -1,13 +1,28 @@
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using catto.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace catto_test;
 
-public class ScoreControllerTest
+public class ScoreControllerTest : IClassFixture<IntegrationFixtures>
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly IntegrationFixtures _fixtures;
 
+    public ScoreControllerTest(IntegrationFixtures fixtures)
+    {
+        _fixtures = fixtures;
+    }
+    
+    [Fact]
+    public async Task Test1()
+    {
+        var client = _fixtures.SetupClient();
+        var response = await client.GetAsync("/api/Score");
+        var scores = System.Text.Json.JsonSerializer.Deserialize<List<Score>>(response.Content.ReadAsStringAsync().Result); 
     }
 }
