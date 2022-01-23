56385788f48a81604facd2af102897cd26a329db
Villou <dkulic01@gmail.com>
2022-01-23 22:34:11 +0100

    fix proxy conf

--- catto/ClientApp/proxy.conf.js
+++ catto/ClientApp/proxy.conf.js
@@ -8,7 +8,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
 
 const PROXY_CONFIG = [
   {
-    context: ["/weatherforecast", "/api"],
+    context: ["/api"],
     target: target,
     secure: false,
   },
