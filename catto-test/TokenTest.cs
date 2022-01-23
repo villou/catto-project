2b731a603161bfe4e5772e23a15bbb3e68a3c5bc
Villou <dkulic01@gmail.com>
2022-01-23 21:52:56 +0100

    TokenTest

---
+++ catto-test/TokenTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using catto.Models;
+using Xunit;
+
+namespace catto_test;
+
+public class TokenTest
+{
+ 
+    [Fact]
+    public void TestToken()
+    {
+        var userId = 1;
+        var token = Token.GenerateToken(userId);
+        Assert.Equal(userId, token.UserId);
+        Assert.True(token.TokenKey.Count() > 10);
+    }
+}
