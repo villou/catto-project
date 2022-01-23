d0b96351062159a2e03b0d9d0a893fb8c8816e29
Villou <dkulic01@gmail.com>
2022-01-23 19:30:10 +0100

    UserControllerTest get/login/register

--- catto-test/IntegrationFixtures.cs
+++ catto-test/IntegrationFixtures.cs
@@ -1,5 +1,4 @@
 using System.Net.Http;
-using System.Text.Json;
 using catto.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -10,50 +9,50 @@ namespace catto_test;
 
 public class IntegrationFixtures : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseSetting("Environment", "Test");
-    }
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
+  {
+    builder.UseSetting("Environment", "Test");
+  }
 
-    public HttpClient SetupClient()
+  public HttpClient SetupClient()
+  {
+    using (var context = GetContext())
     {
-        using (var context = GetContext())
-        {
-            context.Database.EnsureDeleted();
-        }
-        SetupFixtures();
-        return CreateClient();
+      context.Database.EnsureDeleted();
     }
+    SetupFixtures();
+    return CreateClient();
+  }
 
-    public CattoContext GetContext()
+  public CattoContext GetContext()
+  {
+    return Services.CreateScope().ServiceProvider.GetRequiredService<CattoContext>();
+  }
+
+  private void SetupFixtures()
+  {
+    using var context = GetContext();
+    var user = new User
     {
-        return Services.CreateScope().ServiceProvider.GetRequiredService<CattoContext>();
-    }
+      Username = "pascal",
+      Password = "motdepasse",
+      Avatar = "https://imageserver.petsbest.com/marketing/blog/cat-behavior-issues.jpg",
+    };
+    context.Users.Add(user);
+    context.SaveChanges();
 
-    private void SetupFixtures()
+    var score = new Score
     {
-        using var context = GetContext();
-        var user = new User
-        {
-            Username = "pascal",
-            Password = "motdepasse",
-            Avatar = "https://imageserver.petsbest.com/marketing/blog/cat-behavior-issues.jpg",
-        };
-        context.Users.Add(user);
-        context.SaveChanges();
+      Id = 1,
+      UserId = user.Id,
+      Value = 10,
+    };
+    context.Scores.Add(score);
+    context.SaveChanges();
+  }
 
-        var score = new Score
-        {
-            Id = 1,
-            UserId = user.Id,
-            Value = 10,
-        };
-        context.Scores.Add(score);
-        context.SaveChanges();
-    }
-
-    public T? GetBody<T>(HttpResponseMessage response)
-    {
-        return JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().Result);
-    }
+  public T? GetBody<T>(HttpResponseMessage response)
+  {
+    return JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().Result);
+  }
 }
--- catto-test/UserControllerTest.cs
+++ catto-test/UserControllerTest.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using catto.DTO;
 using catto.Models;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace catto_test;
 
@@ -17,9 +17,90 @@ public class UserControllerTest : IClassFixture<IntegrationFixtures>
     {
         _fixtures = fixtures;
     }
-    
+
     [Fact]
-    public async Task GetUserList()
+    public async Task GetUser()
     {
+        var client = _fixtures.SetupClient();
+        var response = await client.GetAsync("/api/User");
+        var user = _fixtures.GetBody<List<User>>(response);
+
+        await using var context = _fixtures.GetContext();
+
+        Assert.True(user?.Count <= 5);
+    }
+
+    [Fact]
+    public async Task Login()
+    {
+        var client = _fixtures.SetupClient();
+        var userDto = new UserDto
+        {
+            Username = "pascal",
+            Password = "motdepasse",
+        };
+
+        var response = await client.PostAsJsonAsync("/api/User/login", userDto);
+        var user = _fixtures.GetBody<UserDto>(response);
+
+        await using var context = _fixtures.GetContext();
+
+        Assert.Equal("pascal", user?.Username);
+        Assert.Equal("motdepasse", user?.Password);
+
+    }
+
+    [Fact]
+    public async Task LoginWrongCredentials()
+    {
+        var client = _fixtures.SetupClient();
+        var userDto = new UserDto()
+        {
+            Username = "pascal",
+            Password = "123456",
+        };
+
+        var response = await client.PostAsJsonAsync("/api/User/login", userDto);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+
+    }
+
+    [Fact]
+    public async Task Register()
+    {
+        var client = _fixtures.SetupClient();
+        var userDto = new UserDto()
+        {
+            Username = "martin",
+            Password = "matin",
+        };
+        await using var context = _fixtures.GetContext();
+        var userCount = context.Users.Count();
+
+        var response = await client.PostAsJsonAsync("/api/User/register", userDto);
+        var user = _fixtures.GetBody<UserDto>(response);
+
+        Assert.Equal("martin", user?.Username);
+        Assert.Equal("matin", user?.Password);
+        Assert.Equal(context.Users.Count(), userCount + 1);
+    }
+
+    [Fact]
+    public async Task RegisterUsernameAlreadyExists()
+    {
+        var client = _fixtures.SetupClient();
+
+        var userDto = new UserDto()
+        {
+            Username = "pascal",
+            Password = "zerozero",
+        };
+        await using var context = _fixtures.GetContext();
+        var userCount = context.Users.Count();
+        var response = await client.PostAsJsonAsync("/api/User/register", userDto);
+
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        Assert.Equal(context.Users.Count(), userCount);
     }
 }
